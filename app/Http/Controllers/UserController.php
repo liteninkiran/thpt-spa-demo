dcd69799f5ef4492681ff453bbfc70eab7be3a20
Kiran Anand <liteninkiran@hotmail.com>
2021-09-07 15:19:02 +0100

    User GET API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function getUsers() {
+        return response()->json(User::all(), 200);
+    }
+
+    public function getUser($id) {
+        $user = User::find($id);
+
+        if(is_null($user)) {
+            return response()->json(['message' => 'User Not Found'], 404);
+        }
+
+        return response()->json($user, 200);
+    }
 }
